fb12eebe928dce0d8f9101e5a0e9573a3653da72
AtiqurRahmanAtik <atikahmedtanvir84@gmail.com>
2025-04-28 23:42:00 +0600

    add some

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,6 +4,8 @@ const About = () => {
     return (
         <div>
             <h1 className="text-center font-bold text-2xl my-7 capitalize text-red-600"> this is about components</h1>
+            
+            <h1 className="text-center font-bold text-2xl my-7 capitalize text-red-600"> this is about components</h1>
         </div>
     );
 };
